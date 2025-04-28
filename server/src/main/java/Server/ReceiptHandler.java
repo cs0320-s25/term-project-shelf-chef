56fe1a9055313fa3b1a56502e604f70576e79098
rma39 <ryan_ma1@brown.edu>
2025-04-27 22:20:03 -0400

    feat-added ocr for receipt scanning

---
+++ server/src/main/java/Server/ReceiptHandler.java
@@ -0,0 +1,17 @@
+package Server;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class ReceiptHandler {
+    
+}
