5197ecb39a8f0c58e960b5a377a5562105f8bd6e
ssaxen14 <simran_saxena@brown.edu>
2025-04-28 15:38:39 -0400

    feat: backend ingredient creator

---
+++ server/src/main/java/Creator/IngredientCreator.java
@@ -0,0 +1,24 @@
+package main.java.Creator;
+
+import CreatedObjects.Ingredient;
+import Exceptions.FactoryFailureException;
+import java.util.List;
+
+public class IngredientCreator implements CreatorFromRow<Ingredient> {
+
+  /**
+   * @param row list of strings from the line that is read from the CSV
+   */
+  @Override
+  public Ingredient create(List<String> row) throws FactoryFailureException {
+    if (row.size() != 3) {
+      throw new FactoryFailureException("Row does not have 3 columns", row);
+    }
+
+    String name = row.get(0);
+    String quantity = row.get(1);
+    String expiration = row.get(2);
+
+    return new Ingredient(name, quantity, expiration);
+  }
+}
