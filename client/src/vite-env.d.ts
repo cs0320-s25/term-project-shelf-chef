58688d59406862c2472b8103029b202e201fd48a
kylewisialowski <103215823+kylewisialowski@users.noreply.github.com>
2025-05-13 21:51:58 -0400

    test: fixed tests for compatibility on re-run

--- client/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  VITE_CLERK_PUBLISHABLE_KEY: any;
-  VITE_APP_NODE_ENV: string;
-  // define more env variables if needed
-}
-
-interface ImportMeta {
-  env: ImportMetaEnv;
-}
